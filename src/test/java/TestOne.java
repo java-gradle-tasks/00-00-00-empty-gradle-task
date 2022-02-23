5c8ada815396094d5bfcf1e7e3496f3aab48a50d
Csaba Gyuris <gycsaba@vasvari.hu>
2022-02-23 10:12:20 +0100

    Test nevének pontosítása

--- src/test/java/TestOne.java
+++ src/test/java/TestOne.java
@@ -1,7 +1,7 @@
    import org.junit.jupiter.api.Assertions;
    import org.junit.jupiter.api.Test;
 
-   public class Test {
+   public class TestOne {
 
       @Test
       public void test() {         
